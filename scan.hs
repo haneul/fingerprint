8becef4e42a29e1ebbc10a194d5d2e74b744a858
Taesoo Kim <taesoo@mit.edu>
2012-12-09 00:54:11 -0500

    http

--- scan.hs
+++ scan.hs
@@ -4,35 +4,47 @@ module Main (main) where
 
 import Control.Concurrent
 import Control.Exception
+import Control.Applicative
 import Control.Monad  
 import Data.Maybe
+import Data.List
+import Data.List.Split
 import Network
 import Network.BSD
+import Network.HTTP hiding (port)
 import System.Environment
 import System.Exit
 import System.IO
 import System.Timeout
-import Data.Text
-import Control.Applicative
+import qualified Data.Text as T
+import Text.Regex.Posix
 
 data Record = Record { ip   :: String
                      , port :: Int
-                     , log  :: String }
+                     , log  :: [String] }
    deriving Show
 
 -- | (port, ms, scan)
-scanOpts = [(22,  500, scanSSH)
-           ,(80, 1500, scanHTTP)]
+scanOpts = [(22, 400, scanSSH)
+           ,(80, 800, scanHTTP)]
 
 main :: IO ()
 main = do
-    getArgs >>= mapM_ (withSocketsDo . doScan)
+    args <- getArgs
+    case args of
+      [host] -> withSocketsDo $ doScan (parseHost host)
+      _      -> usage
 
-doScan :: String -> IO ()
-doScan host =
+usage = do
+  putStrLn "[usage] ip"
+  exitFailure
+  
+doScan :: [String] -> IO ()
+doScan hosts = do
     forM_ scanOpts $ \(port, ms, scan) ->
-      threadWithChannel (withDefault Nothing $ scan host port) ms >>= hitCheck
+      mapM (initScan port ms scan) hosts >>= mapM_ hitCheck
   where
+    initScan port ms scan host = threadWithChannel (withDefault Nothing $ scan host port) ms
     hitCheck mvar = takeMVar mvar >>= maybe (return ()) printHit
     printHit = putStrLn . show
 
@@ -51,25 +63,30 @@ scanSSH host port = do
     hSetBuffering h NoBuffering
     log <- trim <$> hGetLine h
     hClose h
-    return $ Just (Record host port log)
+    return $ Just (Record host port [log])
 
 scanHTTP :: String -> Int -> IO (Maybe Record)
 scanHTTP host port = do
-    h <- connectTo host (PortNumber (fromIntegral port))
-    hSetBuffering h NoBuffering
-    hPutStr h "GET / HTTP/1.1\n\
-              \User-Agent: Mozilla/5.0 (X11; Linux x86_64; rv:17.0) Gecko/20100101 Firefox/17.0\n\
-              \Host: xk\n\
-              \Accept: */*\n\n"
-    -- get until "Server:"
-    log <- trim <$> hGetLine h
--- getHttpHeader ["Server:", "X-Powered-By:", "X-Runtime", "X-Version", "X-AspNet-Version"] hGetLine h
-    hClose h
-    return $ Just (Record host port log)
+    resp <- simpleHTTP (getRequest $ "http://" ++ host ++ ":" ++ show port)
+    return $ either (const Nothing) extractHdrs resp
+  where extractHdrs resp = Just (Record host port $ parseHdrs resp)
+        parseHdrs   resp = map (trim . show) (filterHdrs resp)
+        filterHdrs  resp = filter (\r-> hdrName r `elem` hdrs) (getHeaders resp)
+        hdrs = [HdrServer,
+                HdrCustom "X-Powered-By",
+                HdrCustom "X-Runtime",
+                HdrCustom "X-Version",
+                HdrCustom "X-AspNet-Version"]
 
 withDefault :: a -> IO a -> IO a
 withDefault def action =
     handle (\(e::SomeException) -> return def) action
 
 trim :: String -> String
-trim = unpack . strip . pack
+trim = T.unpack . T.strip . T.pack
+
+parseHost :: String -> [String]
+parseHost host = map (intercalate ".") $ expand (splitOn "." host)
+  where expand [] = [[]]
+        expand (x:xs) | x == "*"  = [x:y| x <- map show [0..255], y <- expand xs]
+                      | otherwise = [x:y| y <- expand xs]
