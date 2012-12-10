d9cc7c7723c70d210c191c532622b188a04dee24
Taesoo Kim <taesoo@mit.edu>
2012-12-09 20:57:47 -0500

    friendly log

--- run.sh
+++ run.sh
@@ -1,11 +1,9 @@
 #!/bin/bash
 
-cabal build
-
 OUT=out-$(date +%F)
 mkdir -p $OUT
 for ip in `seq 1 255`; do
-  echo "BEG: $(date)" > $OUT/log
-  ./dist/build/scan/scan '$ip.*.*.*' 2>$OUT/$ip
-  echo "END: $(date)" > $OUT/log
+  echo "BEG: $(date) $ip.*.*.*" > $OUT/log
+  ./dist/build/scan/scan "$ip.*.*.*" 2>$OUT/$ip
+  echo "END: $(date) $ip.*.*.*" > $OUT/log
 done
