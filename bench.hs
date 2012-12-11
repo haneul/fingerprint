a84ff075d2a9b4ba875459b766d5fbd400a7e681
Taesoo Kim <taesoo@mit.edu>
2012-12-11 15:49:08 -0500

    use printf

--- bench.hs
+++ bench.hs
@@ -4,33 +4,36 @@ module Main (main) where
 import System.Environment
 import System.Exit
 import System.Cmd
+import System.IO
 import System.Locale (defaultTimeLocale)
 import System.Directory
 import Control.Concurrent
 import Control.Monad
 import Data.Time (UTCTime, getCurrentTime, formatTime, diffUTCTime)
 import Data.List
+import Text.Printf
 
 main = do
     args <- getArgs
     case args of
-      [nproc, nsock, ip] -> run (read nproc::Int) (read nsock::Int) ip
+      [nproc, nsock, ip] -> run (read nproc) (read nsock) ip
       _ -> usage
 
+run :: Int -> Int -> String -> IO ()
 run nproc nsock ip = do
   beg <- getCurrentTime
   let dir = formatTime defaultTimeLocale "%F" beg
   createDirectoryIfMissing True dir
 
-  putStrLn $ "Start: " ++ show beg
+  hPrintf stdout "Start: %s\n" (show beg)
   done <- newEmptyMVar
   forM [1..nproc] (forkIO . loop done ip)
   forM [1..nproc] (\_-> takeMVar done)
 
   end <- getCurrentTime
   let diff = diffUTCTime end beg
-  putStrLn $ "End: " ++ show end ++ " @" ++ show diff
-  putStrLn $ "Scanned: " ++ show (nproc * 255 * 255) ++ " = "
+  hPrintf stdout "End: %s @ %s\n" (show end) (show diff)
+  hPrintf stdout "Scanned: %d\n" (nproc * 255 * 255)
   return ()
   where loop done ip id = do
           let from = ip ++ "." ++ show id ++ ".0.0"
--- test.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-ip='143.*.5.*'
-
-echo "BEG: $(date) $ip"
-runhaskell scan.hs "$ip"
-echo "END: $(date) $ip"
