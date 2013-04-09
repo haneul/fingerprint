3c7cbcbbb8e8ef7558cbbdce5dbd0579bbe5e696
Taesoo Kim <taesoo@mit.edu>
2013-04-09 19:18:02 -0400

    run asynchronously

--- planet/do.sh
+++ planet/do.sh
@@ -1,8 +1,32 @@
 #!/bin/bash
 
-VERBOSE=1
+VER=1
+TMP=$(mktemp -d)
+PCS=( $(cat machines) )
 
-for m in $(cat machines); do
-  ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
-    -i syhan.pkey -l uw_scanner $m $@
+log() {
+  if [[ $VER == 1 ]]; then
+    echo "[$(date)] $@"
+  fi
+}
+
+for m in ${PCS[@]}; do
+  log "Run $@ on $m"
+  {
+    BUF=$(mktemp)
+    ssh -q -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no \
+      -i syhan.pkey -l uw_scanner $m $@ &> $BUF
+    mv $BUF $TMP/$m
+    log "Done $m"
+  } &
 done
+
+while true; do
+  DONE=($TMP/*)
+  if [[ ${#DONE[@]} == ${#PCS[@]} ]]; then
+    echo "See. $TMP"
+    exit 0
+  fi
+  sleep 1
+done
+
