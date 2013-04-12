ae1481bc434299f8850480018c609ed009dbc777
Taesoo Kim <taesoo@mit.edu>
2013-04-12 09:45:52 -0400

    set from/to in worker

--- worker.sh
+++ worker.sh
@@ -1,22 +1,24 @@
 #!/bin/bash
 
-if [[ $# < 2 ]]; then
-  echo "[usage] ip out"
+if [[ $# < 3 ]]; then
+  echo "[usage] from to out"
   echo " ex) $0 5 out-$(date +%F)"
   exit 1
 fi
 ulimit -n 1024
 
-IP=$1
-OUT=$2
+IP1=$1
+IP2=$2
+OUT=$3
 
-NC=$(ulimit -n)
+# NC=$(ulimit -n)
+NC=${NC:-1000}
 NC=$(($NC - 48))
 
 echo "[$(date)] Run with $NC fds"
 
 mkdir -p $OUT
 
-echo "BEG: $(date) $IP.*.*.*" >> $OUT/log
-  ./dist/build/scan/scan $NC "$IP.0.0.0" "$IP.255.255.255" &>> $OUT/$IP
-echo "END: $(date) $IP.*.*.*" >> $OUT/log
+echo "BEG: $(date) $IP1 ~ $IP2" >> $OUT/log
+  ./dist/build/scan/scan $NC "$IP1" "$IP2" 2>&1 >> $OUT/$IP1-$IP2
+echo "END: $(date) $IP1 ~ $IP2" >> $OUT/log
